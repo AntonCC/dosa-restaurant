a370b5ddc967dc65d7947a28763ee0f929429f55
Anton <antonchetvv@gmail.com>
2021-01-29 16:25:36 -0500

    Issue with landing resize flicker, switched back to normal sizing

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './landing.scss'
 import debounce from '../../helperFuncs/debounce'
 
 const Landing = ({ bgImage, small }) => {
-  const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+  // Landing size flickers when nav becomes fixed.
 
-  useEffect(() => {
-    window.addEventListener('resize', debouncedHandleHeightChange)
+  // const [innerHeight, setInnerHeight] = useState(window.innerHeight)
 
-    return () => {
-      window.removeEventListener('resize', debouncedHandleHeightChange)
-    }
-  }, [])
+  // useEffect(() => {
+  //   window.addEventListener('resize', debouncedHandleHeightChange)
 
-  const handleHeightChange = () => {
-    setInnerHeight(window.innerHeight)
-  }
+  //   return () => {
+  //     window.removeEventListener('resize', debouncedHandleHeightChange)
+  //   }
+  // }, [])
 
-  const debouncedHandleHeightChange = debounce(handleHeightChange, 75)
+  // const handleHeightChange = () => {
+  //   setInnerHeight(window.innerHeight)
+  // }
+
+  // const debouncedHandleHeightChange = debounce(handleHeightChange, 75)
 
   return (
-    <div className={`landing ${small ? 'small' : ''}`} style={{ background: `url(${bgImage}) center/cover no-repeat`, height: `calc(${innerHeight}px - 84px)` }}>
+    <div className={`landing ${small ? 'small' : ''}`} style={{ background: `url(${bgImage}) center/cover no-repeat`}}>
 
     </div>
   )
