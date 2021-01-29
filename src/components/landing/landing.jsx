a481cbd6a5a5b69c6edc585b8598f38f7c616174
Anton <antonchetvv@gmail.com>
2021-01-29 16:03:41 -0500

    landing height accounts for mobile address bar

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './landing.scss'
+import debounce from '../../helperFuncs/debounce'
 
 const Landing = ({ bgImage, small }) => {
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+
+  useEffect(() => {
+    window.addEventListener('resize', debouncedHandleHeightChange)
+
+    return () => {
+      window.removeEventListener('resize', debouncedHandleHeightChange)
+    }
+  }, [])
+
+  const handleHeightChange = () => {
+    setInnerHeight(window.innerHeight)
+  }
+
+  const debouncedHandleHeightChange = debounce(handleHeightChange, 75)
+
   return (
-    <div className={`landing ${small ? 'small' : ''}`} style={{ background: `url(${bgImage}) center/cover no-repeat` }}>
+    <div className={`landing ${small ? 'small' : ''}`} style={{ background: `url(${bgImage}) center/cover no-repeat`, height: `calc(${innerHeight}px - 84px)` }}>
 
     </div>
   )
